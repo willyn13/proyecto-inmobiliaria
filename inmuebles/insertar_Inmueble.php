d759e45108e1440df595526d7dba6ce2282abcd4
Sebas <Sebas@SEBASTIAN>
2015-11-30 20:22:02 +0100

    29-11-2015

--- inmuebles/insertar_Inmueble.php
+++ inmuebles/insertar_Inmueble.php
@@ -1,6 +1,5 @@
 <?php
-$conexion = mysqli_connect('localhost','root','','inmobiliaria')
-	or die('No se pudo conectar: ' . mysqli_error());
+	$conexion = mysqli_connect('localhost','root','','inmobiliaria') or die('No se pudo conectar: ' . mysqli_error());
 
 	if (mysqli_connect_errno()) {
 		printf("No se pudo conectar: %s/n", mysqli_connect_error());
@@ -15,16 +14,30 @@ $sql = "INSERT INTO inmuebles (idlocalidad, dni_propietario, venta, alquiler, ha
 	 ".$_POST["terraza"].", ".$_POST["trastero"].",".$_POST["piscina"].",".$_POST["garaje"].", '".$_POST["direccion"]."',".$_POST["precio_venta"].", 
 	 ".$_POST["precio_alquiler"].")";
 
-	
-echo $sql;
 
 	$res = mysqli_query($conexion,$sql);							
 	if ($res === TRUE) {
-	   	echo "Se ha insertado el usuario.";
+	   	echo "Se ha insertado el inmueble.";
 		header ("Location: gestiondeinmuebles.php");
 	} else {
-		printf("No se pudo insertar el usuario: 
+		printf("No se pudo insertar el inmueble: 
 		%s\n", mysqli_error($conexion));
 	}
-	mysqli_close($conexion);
+	$ultimoId = mysqli_insert_id();// esto trae el id autoincremetable que se guardo
+	//terminamos de ingresar el inmueble
+	$FormatosPermitidos  = array("image/jpg","image/jpeg","image/gif","image/png");
+	$limiteKb = 1000; // 1000KB = 1MB
+	foreach ($_FILES["imagen"] as $it => $imagen) {
+	//esto es un ciclo que se repite segun la cantidad de datos en el formulario
+		//primero preguntamos si es el formato correcto, y si tiene el tamaño adecuado
+		if(in_array($imagen["type"],$FormatosPermitidos) && $imagen["size"]<$limiteKb * 1024){
+			$path = "imagenes/";
+			$nombreImagen = md5(uniqid(rand(),true)); //nombre aleatorio alfanumerico
+			$PathCompleto = $path.$nombreImagen;
+			if(move_uploaded_file($imagen["tmp_name"], $PathCompleto)){
+				mysqli_query($conexion,"INSERT INTO imagenes(id_inmueble,imagen) values('".$ultimoId."','".$PathCompleto."')");
+			}
+		}
+	}
+
 ?>
