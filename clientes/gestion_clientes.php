b62e52c95cae173794d3d720d42392ea91cdb5ec
Javier <Javier@Core2Quad>
2015-12-06 16:32:58 +0100

    Dejo funcionando el menu de comerciales de gestion de clientes

--- clientes/gestion_clientes.php
+++ clientes/gestion_clientes.php
@@ -38,8 +38,8 @@ echo '</div>';
                 $email=$reg[4];
 
                 $display.="<tr>
-                              <td><a href=\"eliminar_cliente.php?dni_cliente=".$dni_cliente."\"><input type='button' value='Eliminar'></a></td>
-                              <td><a href=\"modificar_cliente.php?dni_cliente=".$dni_cliente."\"><input type='button' value='Modificar'></a></td>
+                              <td><a href=\"clientes/eliminar_cliente.php?dni_cliente=".$dni_cliente."\"><input type='button' value='Eliminar'></a></td>
+                              <td><a href=\"clientes/modificar_cliente.php?dni_cliente=".$dni_cliente."\"><input type='button' value='Modificar'></a></td>
                               <td>".$dni_cliente."</td>	
                               <td>".$nombre."</td>
                               <td>".$apellidos."</td>
