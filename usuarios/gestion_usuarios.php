42828e63b94f24bc3cee6e8b3c427dd3828a0c04
47228571A <47228571A@HLPRPRUEBAS01.salud.madrid.org>
2015-12-10 14:45:37 +0100

    arreglo usuarios y css añadidas

--- usuarios/gestion_usuarios.php
+++ usuarios/gestion_usuarios.php
@@ -1,6 +1,7 @@
 <link type="text/css" rel="stylesheet" href="http://localhost/proyecto-inmobiliaria/css/style.css"/>
 <script type="text/javascript" src="http://localhost/proyecto-inmobiliaria/js/jquery-2.1.4.js"></script>
 <script type="text/javascript" src="http://localhost/proyecto-inmobiliaria/js/navegar.js"></script>
+
 <?php
     echo '<div class="cls_dialog">';
         $conexion = mysqli_connect('localhost','root','','inmobiliaria')
@@ -12,51 +13,48 @@
         }
     echo '</div>';
     
-	$display='<div class="cls_gestiones"><h1>Gestión de Usuarios</h1>';	
-	
-	$consulta = 'SELECT * FROM usuarios';
-	$resultado= mysqli_query($conexion,$consulta)
-	or die('Consulta fallida: ' . mysqli_error());
-	
-	$display.="<table>";
-		if (mysqli_num_rows($resultado)==0 ){
-			$display.="<p class=\"error\"><i>No hay comerciales.</i></p>";
-		} else{
-                        $display.="<a href=\"alta_comercial.php\"><input type='SUBMIT' name='SUBMIT' value='Dar de Alta un Usuario'></a>";
-			$display.="<tr>
-				<th></th>
-				<th></th>
-                                <th>DNI</th>
-                                <th>NOMBRE</th>
-                                <th>APELLIDOS</th>
-                                <th>CARGO</th>
-                                <th>ZONA</th>	
-				</tr>";
-				
+    $display='<div class="cls_gestiones"><h1>Gestión de Usuarios</h1>';	
 
-		while($reg=mysqli_fetch_array($resultado) ){
-			$dni_usuario=$reg[0];
-			$zona=$reg[1];
-			$nombre=$reg[2];
-			$apellidos=$reg[3];
-			$cargo=$reg[4];
-				
-			$display.="<tr>
-				<td><a href=\"eliminar_comercial.php?dni_usuario=".$dni_usuario."\"><input type='button' id='id_eliminar_usuario' value='Eliminar'></a></td>
-				<td><a href=\"modificar_comercial.php?dni_usuario=".$dni_usuario."\"><input type='button' id='id_modificar_usuario' value='Modificar'></a></td>
-				<td>".$dni_usuario."</td>	
-				<td>".$nombre."</td>
-				<td>".$apellidos."</td>
-				<td>".$cargo."</td>
-				<td>".$zona."</td>
+    $consulta = 'SELECT * FROM usuarios';
+    $resultado= mysqli_query($conexion,$consulta)
+    or die('Consulta fallida: ' . mysqli_error());
 
-			</tr>";
-	}
-	
-}
-$display.="</table></div>";
-	mysqli_close($conexion);
-	echo $display
+    $display.="<table>";
+        if (mysqli_num_rows($resultado)==0 ){
+                $display.="<p class=\"error\"><i>No hay comerciales.</i></p>";
+        } else{
+                $display.="<a href=\"alta_comercial.php\"><input type='SUBMIT' name='SUBMIT' value='Dar de Alta un Usuario'></a>";
+                $display.="<tr>
+                        <th></th>
+                        <th></th>
+                        <th>DNI</th>
+                        <th>NOMBRE</th>
+                        <th>APELLIDOS</th>
+                        <th>CARGO</th>
+                        <th>ZONA</th>	
+                        </tr>";
+
+
+        while($reg=mysqli_fetch_array($resultado) ){
+                $dni_usuario=$reg[0];
+                $zona=$reg[1];
+                $nombre=$reg[2];
+                $apellidos=$reg[3];
+                $cargo=$reg[4];
+
+                $display.="<tr>
+                                <td><a href=\"eliminar_comercial.php?dni_usuario=".$dni_usuario."\"><input type='button' id='id_eliminar_usuario' value='Eliminar'></a></td>
+                                <td><a href=\"modificar_comercial.php?dni_usuario=".$dni_usuario."\"><input type='button' id='id_modificar_usuario' value='Modificar'></a></td>
+                                <td>".$dni_usuario."</td>	
+                                <td>".$nombre."</td>
+                                <td>".$apellidos."</td>
+                                <td>".$cargo."</td>
+                                <td>".$zona."</td>
+                          </tr>";
+        }	
+    }
+    
+    $display.="</table></div>";
+    mysqli_close($conexion);
+    echo $display;
 ?>
-	
-	
--- ventas/mostrarventas.php
+++ ventas/mostrarventas.php
@@ -1,77 +1,72 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<link rel=stylesheet href="style.css" type="text/css">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Gestion</title>
-</head>
-</html>
+<link type="text/css" rel="stylesheet" href="http://localhost/proyecto-inmobiliaria/css/style.css"/>
+<script type="text/javascript" src="http://localhost/proyecto-inmobiliaria/js/jquery-2.1.4.js"></script>
+<script type="text/javascript" src="http://localhost/proyecto-inmobiliaria/js/navegar.js"></script>
+
 <?php
-	$conexion = mysqli_connect('localhost','root','','inmobiliaria')
-	or die('No se pudo conectar: ' . mysqli_error());
+    echo '<div class="cls_dialog">';
+        $conexion = mysqli_connect('localhost','root','','inmobiliaria')
+        or die('<h2>No Se Pudo Conectar: </h2>' . mysqli_error());
 
-	if (mysqli_connect_errno()) {
-		printf("No se pudo conectar: %s/n", mysqli_connect_error());
-		exit();
-	}
-session_start();
-$dni= $_SESSION["MM_Username"];
-$sql="select cargo,idzona from usuarios where dni_usuario='".$dni."'";
-$result= mysqli_query($conexion,$sql);
-while($fila = mysqli_fetch_row($result)){
-       $cargo=$fila[0];
-       }
-       if($cargo=="admin"){
-		   $consulta = "SELECT * FROM ventas";        
-       }else{
-		   $sql1="select zona from usuarios where dni='".$dni."'";
-		   $result1= mysqli_query($conexion,$sql1);
-		   while($fila = mysqli_fetch_row($result1)){
-               $zona=$fila[0];
-			}	
-       $consulta = "select * from ventas where dni_usuario ='".$dni."'";
-       }
-	
-	$display="";
+        if (mysqli_connect_error()) {
+                printf('<h2>No Se Pudo Conectar: %s/n</h2>', mysqli_connect_error());
+                exit();
+        }
+    echo '</div>';
 
-$resultado1=mysqli_query($conexion,$consulta);
+    $display = '<div class="cls_gestiones"><h1>Gestión de Crompra-Venta</h1>';	
+    
+    session_start();
+    
+    $dni = $_SESSION["MM_Username"];
+    $sql = "SELECT cargo,idzona FROM usuarios WHERE dni_usuario='".$dni."'";
+    $result = mysqli_query($conexion,$sql);
+    
+    while($fila = mysqli_fetch_row($result)){
+        $cargo=$fila[0];
+    }
+    
+    if($cargo == "admin"){
+        $consulta = "SELECT * FROM ventas";        
+    } else {
+        $sql1 = "SELECT zona FROM usuarios WHERE dni='".$dni."'";
+        $result1 = mysqli_query($conexion,$sql1);
+        
+        while($fila = mysqli_fetch_row($result1)){
+            $zona = $fila[0];
+        }	
+        $consulta = "SELECT * FROM ventas WHERE dni_usuario ='".$dni."'";
+    }
 
+    $resultado1 = mysqli_query($conexion,$consulta);
 
-while($fila = mysqli_fetch_row($resultado1)){
-$IDCASA = $fila[0];
-$FECHACOMPRA = $fila[2];
-$PRECIOVENTA = $fila[3];
-$DNICOMPRADOR = $fila[1];
-$display.= "<table id='tabladatos' border='1'> \n
-			<tr>
-			<td>&nbsp</td>
-			<td>&nbsp</td>
-			<td>ID CASA</td>
-			<td>FECHA DE COMPRA</td>
-			<td>PRECIO DE VENTA</td>
-			<td>DNI DEL COMPRADOR</td>
-			
-			
-			
-</tr>";
-$display.="
-
- <tr>
-<td width='40' align='left' ><A HREF=\"eliminarventas.php?IDCASA=".$IDCASA."&&FECHACOMPRA=".$FECHACOMPRA."\">Eliminar venta</A></td>
-<td width='40' align='left' ><A HREF=\"formulariomodificarventas.php?IDCASA=".$IDCASA."&&FECHACOMPRA=".$FECHACOMPRA."&&PRECIOVENTA=".$PRECIOVENTA."&&DNICOMPRADOR=".$DNICOMPRADOR."\">Modificar venta</A></td>
-<td width='40' align='center' >".$IDCASA."</td>
-<td width='40' align='center' >".$FECHACOMPRA."</td>
-<td width='40' align='center' >".$PRECIOVENTA."</td>
-<td width='40' align='center' >".$DNICOMPRADOR."</td>
-</tr>";
-}
-$display.="</table>
-";
-echo $display;
-echo "<a href='formularioaltaventa.php'>ALTA DE VENTAS</a>";
-
-
-
-echo "<P><a href=\"principal.php\"><img src='home.png' alt='arriba' WIDTH='100' HEIGHT='100' border='0'>";
-echo "</br>";
+    while($fila = mysqli_fetch_row($resultado1)){
+        $IDCASA = $fila[0];
+        $FECHACOMPRA = $fila[2];
+        $PRECIOVENTA = $fila[3];
+        $DNICOMPRADOR = $fila[1];
+        $display.= "<table id='tabladatos' border='1'> \n
+                        <tr>
+                            <th>&nbsp</th>
+                            <th>&nbsp</th>
+                            <th>ID CASA</th>
+                            <th>FECHA DE COMPRA</th>
+                            <th>PRECIO DE VENTA</th>
+                            <th>DNI DEL COMPRADOR</th>
+                        </tr>";
+        $display.="<tr>
+                        <td><A HREF=\"eliminarventas.php?IDCASA=".$IDCASA."&&FECHACOMPRA=".$FECHACOMPRA."\">Eliminar venta</A></td>
+                        <td><A HREF=\"formulariomodificarventas.php?IDCASA=".$IDCASA."&&FECHACOMPRA=".$FECHACOMPRA."&&PRECIOVENTA=".$PRECIOVENTA."&&DNICOMPRADOR=".$DNICOMPRADOR."\">Modificar venta</A></td>
+                        <td>".$IDCASA."</td>
+                        <td>".$FECHACOMPRA."</td>
+                        <td>".$PRECIOVENTA."</td>
+                        <td>".$DNICOMPRADOR."</td>
+                  </tr>";
+    }
+    
+    $display.="</table></div>";
+    
+    echo $display;
+    echo "<a href='formularioaltaventa.php'>ALTA DE VENTAS</a>";
+    echo "<P><a href=\"principal.php\"><img src='home.png' alt='arriba' WIDTH='100' HEIGHT='100' border='0'>";
+    echo "</br>";
 ?>
