46190a41f50b9a3e405a719c23cc5f7ea646140d
Sebastian <>
2015-12-10 11:25:51 +0100

    10-12-2015

--- clientes/modificar_cliente.php
+++ clientes/modificar_cliente.php
@@ -56,8 +56,8 @@
                 <td><input type="text" id="email" placeholder="Escribe Email" name="email" maxlength="60" value="<?php echo $cliente[4] ?>" required/></td>
             </tr>	
         </table>
-        <input type="button" id="id_modificar" value="Guardar cambios" name="modificar" onclick="modificarCliente('clientes/actualizar_cliente.php')" />
-        <input type="button" value="Cancelar" onclick="ajaxSinFormulario('1','clientes/gestion_clientes.php')">
+        <input type="button" class='cls_buttons' id="id_modificar" value="Guardar cambios" name="modificar" onclick="modificarCliente('clientes/actualizar_cliente.php')" />
+        <input type="button" class='cls_buttons' value="Cancelar" onclick="ajaxSinFormulario('1','clientes/gestion_clientes.php')">
     </form>
 </table>
 </div>
