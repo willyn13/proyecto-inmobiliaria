a2003a8747eff763638022a41a0b038caa970be0
Sebas <Sebas@192.168.56.1>
2015-12-15 22:20:55 +0100

    ULTIMO DIA

--- inmuebles_clientes/fichacasa.php
+++ inmuebles_clientes/fichacasa.php
@@ -4,52 +4,52 @@
 <script type="text/javascript" src="/proyecto-inmobiliaria/js/inmobiliaria.js"></script>
 
 <?php
-    session_start();
-    $dni = $_SESSION["dni"];
+session_start();
+$dni = $_SESSION["dni"];
 
-    echo '<div class="cls_dialog">';
-        $conexion = mysqli_connect('localhost','root','','inmobiliaria')
-        or die('<h2>No Se Pudo Conectar: </h2>' . mysqli_error());
+echo '<div class="cls_dialog">';
+$conexion = mysqli_connect('localhost','root','','inmobiliaria')
+or die('<h2>No Se Pudo Conectar: </h2>' . mysqli_error());
 
-        if (mysqli_connect_error()) {
-            printf('<h2>No Se Pudo Conectar: %s/n</h2>', mysqli_connect_error());
-            exit();
-        }
-        
-    echo '</div>';
-    
-    $sqlMostrar = "SELECT `idcasa`, `venta`, `alquiler`, `habitaciones`, `m2`, `banios`, `terraza`, `trastero`, `piscina`, `garaje`, `direccion`, `precio_venta`, `precio_alquiler`, `localidad`, `provincia` 
-        FROM `inmuebles` , `localidades`, `provincias`
-        WHERE `inmuebles`.`idlocalidad`=`localidades`.`idlocalidad` and `localidades`.`idprovincia`=`provincias`.`idprovincia` and `inmuebles`.`idcasa`= '".$_GET['idcasa']."'";	
+if (mysqli_connect_error()) {
+printf('<h2>No Se Pudo Conectar: %s/n</h2>', mysqli_connect_error());
+exit();
+}
 
-    $resultado = mysqli_query($conexion,$sqlMostrar);
+echo '</div>';
 
-    while ($inmueble = mysqli_fetch_array($resultado)){
-	if($inmueble['piscina']==0) {$piscina="no";} else {$piscina="si";}
-	if($inmueble['terraza']==0) {$terraza="no";} else {$terraza="si";}
-	if($inmueble['trastero']==0) {$trastero="no";} else {$trastero="si";}
-	if($inmueble['garaje']==0) {$garaje="no";} else {$garaje="si";}
-	$idcasa = $inmueble['idcasa'];
+$sqlMostrar = "SELECT `idcasa`, `venta`, `alquiler`, `habitaciones`, `m2`, `banios`, `terraza`, `trastero`, `piscina`, `garaje`, `direccion`, `precio_venta`, `precio_alquiler`, `localidad`, `provincia` 
+FROM `inmuebles` , `localidades`, `provincias`
+WHERE `inmuebles`.`idlocalidad`=`localidades`.`idlocalidad` and `localidades`.`idprovincia`=`provincias`.`idprovincia` and `inmuebles`.`idcasa`= '".$_GET['idcasa']."'";	
 
-			echo "<TABLE BORDER=1 WIDTH=300>";
-			echo "<tr>";
-			echo "<td colspan=\"3\"><p> Precio de venta: " .$inmueble['precio_venta']."</p>";
-			echo "<p> Precio de alquiler: " .$inmueble['precio_alquiler']."</p>";
-			echo "<p> El inmueble consta de: " .$inmueble['habitaciones']." habitaciones, ".$inmueble['banios']." baños y se encuentra en la calle ".$inmueble['direccion']." en la comunidad de ".
-			  $inmueble['localidad']." de la provincia de ".$inmueble['provincia']."</p>";
-			echo "<p>Piscina: ".$piscina;
-			echo "<p>Terraza: ".$terraza;
-			echo "<p>Trastero: ".$trastero;
-			echo "<p>Garaje: ".$garaje;
-			echo "</td>";
-			echo "</tr>";			
-			echo "<tr>";
-			echo "<td colspan=\"3\"><a href=\"gestiondeinmuebles.php?idcasa=".$idcasa."\">Volver</a></td>";
-			echo "</tr>";
-			echo "</TABLE>";
-    }
+$resultado = mysqli_query($conexion,$sqlMostrar);
 
-    $display.="</table></div>";
-    mysqli_close($conexion);
-    echo $display;
+while ($inmueble = mysqli_fetch_array($resultado)){
+if($inmueble['piscina']==0) {$piscina="no";} else {$piscina="si";}
+if($inmueble['terraza']==0) {$terraza="no";} else {$terraza="si";}
+if($inmueble['trastero']==0) {$trastero="no";} else {$trastero="si";}
+if($inmueble['garaje']==0) {$garaje="no";} else {$garaje="si";}
+$idcasa = $inmueble['idcasa'];
+
+echo "<TABLE BORDER=1 WIDTH=300>";
+echo "<tr>";
+echo "<td colspan=\"3\"><p> Precio de venta: " .$inmueble['precio_venta']."</p>";
+echo "<p> Precio de alquiler: " .$inmueble['precio_alquiler']."</p>";
+echo "<p> El inmueble consta de: " .$inmueble['habitaciones']." habitaciones, ".$inmueble['banios']." baños y se encuentra en la calle ".$inmueble['direccion']." en la comunidad de ".
+$inmueble['localidad']." de la provincia de ".$inmueble['provincia']."</p>";
+echo "<p>Piscina: ".$piscina;
+echo "<p>Terraza: ".$terraza;
+echo "<p>Trastero: ".$trastero;
+echo "<p>Garaje: ".$garaje;
+echo "</td>";
+echo "</tr>";			
+echo "<tr>";
+echo "<td colspan=\"3\"><a href=\"gestiondeinmuebles.php?idcasa=".$idcasa."\">Volver</a></td>";
+echo "</tr>";
+echo "</TABLE>";
+}
+
+$display.="</table></div>";
+mysqli_close($conexion);
+echo $display;
 ?>
